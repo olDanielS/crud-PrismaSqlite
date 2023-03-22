5d104b320546eb596fe2bae89e991928f8dae4b7
olDaniels <danielnrt80@gmail.com>
2023-03-21 23:54:42 -0300

    finalized user controler crud

--- src/Controllers/UserController.js
+++ src/Controllers/UserController.js
@@ -47,5 +47,57 @@ export default {
         }catch(error){
             	return res.json(error)
         }
+    },
+    async updateUser(req, res){
+        try{
+            const {email} = req.params;
+            const {name, newEmail} = req.body
+
+            let user = await prisma.user.findUnique({where: {email}});
+            
+
+            if(!user){
+                return res.json({error: "User not found"})
+            }
+
+            const id = user.id;
+
+            user = await prisma.user.update({
+                where: {id: Number(id)},
+                data: {
+                    name,
+                    email: newEmail
+                }
+            })
+            console.log({id})
+            return res.json(user)
+
+
+        }catch(error){
+            res.json(error)
+        }
+    },
+
+    async removeUser(req, res){
+        try{
+            const {email} = req.params;
+        
+            let user = await prisma.user.findUnique({where: {email}});
+            
+
+            if(!user){
+                return res.json({error: "User not found"})
+            }
+
+            const id = user.id;
+
+            user = await prisma.user.delete({where: {id: Number(id)}})
+            console.log({id})
+            return res.json(user ? "User removed" : "some error ocurredy ")
+
+
+        }catch(error){
+            res.json(error)
+        }
     }
 }
--- src/routers.js
+++ src/routers.js
@@ -5,7 +5,10 @@ import UserController from './Controllers/UserController';
 const router = Router();
 
 router.post("/user", UserController.createUser)
-router.get("/user/:email", UserController.findUserByEmail)
 router.get("/users", UserController.findUsers)
+router.get("/user/:email", UserController.findUserByEmail)
+
+router.put("/user/:email", UserController.updateUser)
+router.delete("/user/:email", UserController.removeUser)
                                             
 export {router}
